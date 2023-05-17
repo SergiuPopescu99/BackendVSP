cb4f97845fa1242faa9e18828f49496c9f150034
SergiuPopescu99 <sergiopop25@yahoo.com>
2023-05-18 01:43:10 +0300

    added userROUTER

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const currenciesRouter = require('./routes/currencies.router');
+const userRouter = require('./routes/user.router');
 const api = express.Router();
 api.use('/currencies', currenciesRouter);
-
+api.use('/user', userRouter);
 module.exports = api;
